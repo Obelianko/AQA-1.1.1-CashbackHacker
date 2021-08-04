21b7c5b1cbcbdfea6066f4bca050ff82a66bc0d4
Obelianko <bianka77@inbox.ru>
2021-08-04 15:36:08 +0300

    test update

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -13,7 +13,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(amount);
         int expected = 100;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -22,7 +22,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(amount);
         int expected = 313;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -31,7 +31,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(amount);
         int expected = 0;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -40,7 +40,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(amount);
         int expected = 500;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -49,7 +49,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(amount);
         int expected = 644;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
 
